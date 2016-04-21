43633184dbfd1b80c040358d5686bcfc6f4cc550
Bartek1 <Bartek1@Bartek>
2016-04-21 17:54:46 +0200

    first commit

---
+++ src/main/java/com/example/RealLeftApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealLeftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealLeftApplication.class, args);
+	}
+}
